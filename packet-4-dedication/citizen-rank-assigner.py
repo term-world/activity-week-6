9fc321f080733e3e6e362d771ccf4403c50935fc
normile01@allegheny.edu <normile01@allegheny.edu>
2022-07-14 19:44:01 +0000

    Edit to presentation of second activity

--- packet-4-dedication/citizen-rank-assigner.py
+++ packet-4-dedication/citizen-rank-assigner.py
@@ -1,30 +1,36 @@
 import json
 
 
-def grab_data(filename: str) -> dict[str, dict[str, int]]:
+def grab_file(filename: str) -> dict[str, dict[str, int]]:
     file = open(filename, "r")
-    data = json.load(file)
-    return data
+    contents = json.load(file)
+    return contents
 
 
-# TO-DO: CREATE A FUNCTION THAT EVALUATES A SPECIFIC CITIZEN AGAINST ONE OF THE FOUR SPECIFIC METRICS
+def evaluate_one(citizen_id: str, metric: str, citizen_data: dict[str, dict[str, int]], minimum_specs: dict[str, dict[str, int]]) -> int:
+    
+    # TO-DO: IMPLEMENT FUNCTION THAT EVALUATES A SPECIFIC CITIZEN AGAINST ONE OF THE FOUR SPECIFIC METRICS
 
 
-# TO-DO: CREATE A FUNCTION THAT CALLS THE ABOVE FUNCTION TO EVALUATE A SPECIFIC CITIZEN AGAINST *ALL* FOUR METRICS
+    return rank
+    
+
+def evaluate_all(citizen_id: str, citizen_data: dict[str, dict[str, int]], minimum_specs: dict[str, dict[str, int]]) -> int:
+    
+    # TO-DO: IMPLEMENT FUNCTION THAT EVALUATES A SPECIFIC CITIZEN AGAINST *ALL* FOUR METRICS
+
+
+    return final_rank
 
 
 def main():
-    # DO NOT TOUCH CODE BLOCK BELOW
+    citizen_data = grab_file("data/citizen-data.json")
+    minimum_specs = grab_file("data/minimum-specs.json")
     print()
     citizen_num = input("Select a two-digit citizen ID# [00-47] to determine rank for: ")
     citizen_full_id = "ID#" + citizen_num
+    rank = evaluate_all(citizen_full_id, citizen_data, minimum_specs)
     print()
-    # DO NOT TOUCH CODE BLOCK ABOVE
-    
-    # TO-DO: CALL THE FUNCTION TO EVALUATE THE CHOSEN CITIZEN AGAINST ALL FOUR METRICS AND DETERMINE THEIR RANK
-    
-
-    # DO NOT TOUCH BELOW PRINT STATEMENTS
     print(f"Citizen {citizen_num} has been assigned rank {rank}.")
     print()
 
